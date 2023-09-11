90544a9ad5320aff7134d0e252b3aeb20310c5ae
Kevin Ireri <chosenireri42@gmail.com>
2023-09-11 23:59:05 +0300

    commit

--- src/components/Analytics.tsx
+++ src/components/Analytics.tsx
@@ -9,7 +9,7 @@ const Analytics = () => {
                 <p className='text-green-500 uppercase font-bold'>Data Analytics Dashboard </p>
                 <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2 '>Manage Data Analytics Centrally </h1>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod qui eum et maiores est, laborum ea ipsam culpa quibusdam minus! Nobis nisi corporis tempora illum voluptatem delectus consequuntur eaque veniam?</p>
-                <button className='bg-black w-[200px] rounded-md font-medium py-3 px-4 text-green-400 my-6 m-auto md:mx-0 hover:bg-black/90 ease-in-out'>Get Started</button>
+                <button className='bg-black w-[200px] rounded-md font-medium py-3 px-5 text-green-400 my-6 m-auto md:mx-0 hover:bg-black/90 ease-in-out'>Get Started</button>
             </div>
         </div>
        
